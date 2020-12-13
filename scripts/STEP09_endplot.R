d3cf92054e4ac90220ef200e546e8bca9a01b600
Jimmyzhang1911 <jmzhang1911@gmail.com>
2020-12-14 00:03:55 +0800

    add

--- scripts/STEP09_endplot.R
+++ scripts/STEP09_endplot.R
@@ -1,5 +1,211 @@
 source('Utils.R')
 
+
+myenrichplot <- function(df, title, prob = FALSE){
+  if(length(prob) > 0){
+    ggdata <- df %>%
+      mutate(enrich_type = factor(enrich_type,
+                                  levels = c('GO-CC-enrichment',
+                                             'GO-BP-enrichment',
+                                             'KEGG-enrichment'))) %>%
+      dplyr::filter(p.adjust < 0.05,
+                    Description %in% prob) %>%
+      group_by(enrich_type) %>% 
+      arrange(enrich_type) 
+  }
+  else{
+    ggdata <- df %>%
+      mutate(enrich_type = factor(enrich_type, 
+                                  levels = c('GO-CC-enrichment',
+                                             'GO-BP-enrichment',
+                                             'KEGG-enrichment'))) %>%
+      dplyr::filter(p.adjust < 0.05) %>%
+      group_by(enrich_type) %>% 
+      slice_max(Count, n = 3) %>%
+      arrange(enrich_type)
+  }
+  
+  ggdata2 <- ggdata %>% 
+    mutate(Description = factor(Description, levels = Description),
+           pv = 0.05 - p.adjust)
+  
+  p <- ggplot(data = ggdata2,
+              aes(x = log2(Count+1),
+                  y = Description)) +
+    geom_col(aes(fill = enrich_type,
+                 alpha = p.adjust)) +
+    scale_fill_nejm() +
+    scale_alpha_continuous(range = c(0.4, 1)) +
+    labs(x = 'log2(count+1)',
+         y = '',
+         title = title) +
+    scale_x_continuous(expand = c(0, 0),
+                       limits = c(0, 7)) +
+    theme_bw()
+  
+  return(p)
+}
+
+
+myenrichgetdf <- function(genelist, title){
+  filename <- basename(title)
+  genelist = trygenes$gene_id
+  filename = 'try'
+  gene.df <- bitr(genelist, fromType = "ENSEMBL",
+                  toType = c("SYMBOL", "ENTREZID"),
+                  OrgDb = org.Mm.eg.db)
+  go_cc <- enrichGO(gene       = gene.df$ENSEMBL,
+                    OrgDb      = org.Mm.eg.db,
+                    keyType    = 'ENSEMBL',
+                    ont        = "CC",
+                    pAdjustMethod = "BH",
+                    pvalueCutoff = 0.1,
+                    qvalueCutoff = 0.05) %>%
+    as.data.frame() %>%
+    dplyr::tbl_df() %>%
+    mutate(group = filename,
+           enrich_type = 'GO-CC-enrichment')
+  
+  go_bp <- enrichGO(gene       = gene.df$ENSEMBL,
+                    OrgDb      = org.Mm.eg.db,
+                    keyType    = 'ENSEMBL',
+                    ont        = "BP",
+                    pAdjustMethod = "BH",
+                    pvalueCutoff = 0.1,
+                    qvalueCutoff = 0.05) %>%
+    as.data.frame() %>%
+    dplyr::tbl_df() %>%
+    mutate(group = filename,
+           enrich_type = 'GO-BP-enrichment')
+  go_mf <- enrichGO(gene       = gene.df$ENSEMBL,
+                    OrgDb      = org.Mm.eg.db,
+                    keyType    = 'ENSEMBL',
+                    ont        = "MF",
+                    pAdjustMethod = "BH",
+                    pvalueCutoff = 0.1,
+                    qvalueCutoff = 0.05) %>%
+    as.data.frame() %>%
+    dplyr::tbl_df() %>%
+    mutate(group = filename,
+           enrich_type = 'GO-BP-enrichment')
+  
+  kk <- enrichKEGG(gene = gene.df$ENTREZID,
+                   organism = 'mmu',
+                   pvalueCutoff = 0.05) %>% 
+    as.data.frame() %>%
+    dplyr::tbl_df() %>%
+    mutate(group = filename,
+           enrich_type = 'KEGG-enrichment')
+  tmp <- rbind(go_cc, go_bp)
+  tmp2 <- rbind(kk, go_mf)
+  tmp3 <- rbind(tmp, tmp2)
+  return(tmp2)
+}
+
+
+load('outcomes/masigpro/dynamicdf.RData')
+
+trygenes <- dynamicdf[dynamicdf$cluster == '2',]
+myenrich(trygenes$gene_id,title = 'tmp')
+tmpdf <- myenrichgetdf(trygenes$gene_id, title = 'tmp')
+View(tmpdf)
+
+
+
+
+go_all <- enrichGO(gene       = gene.df$ENSEMBL,
+                  OrgDb      = org.Mm.eg.db,
+                  keyType    = 'ENSEMBL',
+                  ont        = "ALL",
+                  pAdjustMethod = "BH",
+                  pvalueCutoff = 0.1,
+                  qvalueCutoff = 0.05) 
+kk <- enrichKEGG(gene = gene.df$ENTREZID,
+                 organism = 'mmu',
+                 pvalueCutoff = 0.05) %>% 
+  as.data.frame() %>%
+  dplyr::tbl_df() %>%
+  mutate(enrich_method = 'KEGG') %>%
+  dplyr::select(enrich_method,ID:Count)
+View(kk)
+
+go_res <- as.data.frame(go_all) %>% as.data.frame() %>% 
+  tbl_df() %>% rename(enrich_method = ONTOLOGY)
+View(go_res)
+
+df_all <- union_all(kk, go_res)
+View(df_all)
+
+tmpprob <- sample(unique(df_all$ID),8)
+
+getdf <- function(genelist, prob){
+  genelist = trygenes$gene_id
+  prob = tmpprob
+  
+  gene.df <- bitr(genelist, fromType = "ENSEMBL",
+                  toType = c("SYMBOL", "ENTREZID"),
+                  OrgDb = org.Mm.eg.db)
+  
+  kk_res <- enrichKEGG(gene = gene.df$ENTREZID,
+                       organism = 'mmu',
+                       pvalueCutoff = 0.05) %>%
+    as.data.frame() %>%
+    dplyr::tbl_df() %>%
+    mutate(enrich_method = 'KEGG') %>%
+    dplyr::select(enrich_method,ID:Count)
+  
+  go_res <- enrichGO(gene       = gene.df$ENSEMBL,
+                     OrgDb      = org.Mm.eg.db,
+                     keyType    = 'ENSEMBL',
+                     ont        = "ALL",
+                     pAdjustMethod = "BH",
+                     pvalueCutoff = 0.1,
+                     qvalueCutoff = 0.05) %>%
+    as.data.frame() %>% 
+    tbl_df() %>% rename(enrich_method = ONTOLOGY)
+  
+  df_all <- union_all(kk, go_res)
+  
+  adf <- dplyr::filter(df_all, ID %in% prob) %>% dplyr::count(enrich_method) %>%
+    mutate(get = if_else(n >=10, 0, 10-n))
+  
+  bdf <- dplyr::filter(df_all, ID %in% prob)
+  cdf <- left_join(df_all, adf, by = 'enrich_method') %>% dplyr::select(-n) %>%
+    dplyr::filter(ID %!in% prob) %>% group_by(enrich_method) %>% sample_n(get) %>%
+    union_all(bdf) %>% 
+    mutate(Description = factor(Description, levels = unique(Description)))
+}
+
+View(cdf)
+
+
+
+ggdata2 <- df_all %>% 
+  mutate(Description = factor(Description, levels = unique(Description))) %>%
+  group_by(enrich_method) %>% slice_head(n = 10)
+
+ggplot(data = cdf,
+            aes(x = log2(Count+1),
+                y = Description)) +
+  geom_col(aes(fill = enrich_method,
+               alpha = p.adjust)) +
+  scale_fill_nejm() +
+  scale_alpha_continuous(range = c(1, 0.4)) +
+  labs(x = 'log2(count+1)',
+       y = '') +
+  scale_x_continuous(expand = c(0, 0),
+                     limits = c(0, 10)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 12, face = 'bold'),
+        axis.text.y = element_text(size = 8, face = 'bold'))
+
+View(ggdata2)
+
+
+
+
+
+
 load('outcomes/inputdata/input.RData')
 lnclist <- input_matrix_count$lnc_genelist
 
